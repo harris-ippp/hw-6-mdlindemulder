dd02a198e9bbc2255d2db61aae9b3eb0a3606a3e
mdlindemulder <32286519+mdlindemulder@users.noreply.github.com>
2017-11-07 16:53:38 -0600

    Add files via upload
    saved

--- e1.py
+++ e1.py
@@ -8,7 +8,6 @@
 #print(s, t)
 
 from bs4 import BeautifulSoup as bs
-import pandas as pd
 import requests
 ELECTION_ID = []
 url_va = "http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2016/office_id:1/stage:General"
@@ -19,14 +18,14 @@ tags = soup.find_all('tr', 'election_item')
 for t in tags:
     year = t.td.text
     year_id = t['id'][-5:]
-    b = [year, year_id]
+    b = year, year_id
     ELECTION_ID.append(b)
-print(ELECTION_ID)
+#print(ELECTION_ID)
 
 with open('ELECTION_ID','w') as ELECTION_ID_file:
     for line in ELECTION_ID:
-        ELECTION_ID_file.write(line[0] + ' ' + line[1])
-        print(line[0],line[1])
+        ELECTION_ID_file.write(line[0] + ' ' + line[1] + '\n')
+        #print(line[0],line[1])
 
 #lastyear = tags[0]
 #lastyear_id = lastyear['id']
--- e2.py
+++ e2.py
@@ -11,22 +11,7 @@ for line in open("ELECTION_ID", "r"):
         out.write(lastyear_text)
 
 
-#from bs4 import BeautifulSoup as bs
-#import requests
-#ELECTION_ID = []
-#url_va = "http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2016/office_id:1/stage:General"
-#resp = requests.get(url_va)
-#html_va = resp.content
-#soup = bs(html_va, 'html.parser')
 
-#tags = soup.find_all('tr', 'election_item')
-
-#for t in tags:
-#    year = t.td.text
-#    year_id = t['id'][-5:]
-#    b = [year, year_id]
-#    ELECTION_ID.append(b)
-#    print(year, year_id)
 
 
 #for id in open("ELECTION_ID.txt", "r"):
@@ -38,16 +23,9 @@ for line in open("ELECTION_ID", "r"):
 #    with open(file_name, "w") as out:
 #        out.write(lastyear_text)
 
-#d = dict(zip(year_id, year))
 
-#for id in year_id:
-#    base = 'http://historical.elections.virginia.gov/elections/download/{}/precincts_include:0/'
-#    lastyear_url = base.format(id)
-#    lastyear_text = requests.get(lastyear_url).text
-#    file_name = d[id] + ".csv"
 
-#    with open(file_name, 'w') as output:
-#        output.write(lastyear_text)
+
 
 #with open('president_general_2016.csv', 'w') as output:
 #    output.write(requests.get(lastyear_url).text)
